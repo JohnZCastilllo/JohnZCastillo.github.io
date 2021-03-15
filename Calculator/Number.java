0668830da8cea724b5ebd9e129b605b8f696ded1
JohnZCastilllo <80296634+JohnZCastilllo@users.noreply.github.com>
2021-03-15 22:35:46 +0800

    Delete Number.java

--- Calculator/Number.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
-
-public class Number {
-    public static  BigDecimal newNumber(String number){
-    	return new BigDecimal(number);
-    }        
-}
