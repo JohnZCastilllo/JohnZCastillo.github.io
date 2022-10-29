3487e6f1b8f1b6081f570f71312409a6c24704eb
John Z Castillo <johnzunigacastillo@gmail.com>
2022-10-30 00:32:15 +0800

    prepare for local

---
+++ Calculator/calulator/ActionType.java
@@ -0,0 +1,7 @@
+/*
+ * Represent type of action
+ * 
+ */
+public enum ActionType {
+    ADD, SUBTRACT, MULTIPLY, PERCENT, DIVIDE;
+}
--- Calculator/calulator/DisplayField.java
+++ Calculator/calulator/DisplayField.java
@@ -7,29 +7,29 @@ public class DisplayField {
 
 	private static JTextField textField;
 	private static DisplayField displayField;
-	
-    private  DisplayField() {
-    	textField = new JTextField(10);
-    	textField.setMargin(new Insets(25, 0, 25, 0));
-    	textField.setEditable(false);
+
+	private DisplayField() {
+		textField = new JTextField(10);
+		textField.setMargin(new Insets(25, 0, 25, 0));
+		textField.setEditable(false);
 		textField.setBackground(Color.WHITE);
-    	textField.setFont(new Font("Arial", Font.PLAIN, 40));
-    }
-    
-    public static DisplayField getInstance(){
-    	if(displayField == null){
-    		displayField = new DisplayField();
-    		return displayField;
-    	}else{
-    		return displayField;
-    	}
-    }
-    
-   public static void display(String text){
-   		textField.setText(text);
-   }
-   
-   public static JTextField getTextField(){
-   		return textField;
-   }
+		textField.setFont(new Font("Arial", Font.PLAIN, 40));
+	}
+
+	public static DisplayField getInstance() {
+
+		if (displayField == null) {
+			displayField = new DisplayField();
+		}
+
+		return displayField;
+	}
+
+	public void display(String text) {
+		textField.setText(text);
+	}
+
+	public JTextField getTextField() {
+		return textField;
+	}
 }
